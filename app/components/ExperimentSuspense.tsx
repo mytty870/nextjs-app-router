856075af97d7e71822441ec00cc5d11b73f7d217
github mytty870 <otani11ace@icloud.com>
2024-01-26 21:06:23 +0900

    change sleep logic

--- app/components/ExperimentSuspense.tsx
+++ app/components/ExperimentSuspense.tsx
@@ -1,12 +1,8 @@
-function sleep(ms: number ) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms)
-    return (
-      <p>aaa</p>
-    )
-  })
-}
+const sleep = (waitTime: number) => new Promise( resolve => setTimeout(resolve, waitTime) );
 
-export const ExperimentSuspense = () => {
-  throw sleep(1)
+export const ExperimentSuspense = async() => {
+   await sleep(5000)
+   return (
+    <p>解決したよ</p>
+   )
 }
