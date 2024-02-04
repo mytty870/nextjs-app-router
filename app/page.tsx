6d638355c9dc6edbcc74bb79a4ce2cab470ba47c
github mytty870 <otani11ace@icloud.com>
2024-02-04 18:05:36 +0900

    data fetching using poke api

--- app/page.tsx
+++ app/page.tsx
@@ -1,96 +1,31 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 
-export default function Home() {
+async function getPokemon() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+
+  // ビルド時とリクエスト時にfetchが行われ、返ってきた値をサーバー上のデータキャッシュで自動的にキャッシュする
+  // const res = await fetch('https://pokeapi.co/api/v2/pokemon/1', { cache: 'force-cache' })
+
+  // リクエストに対してキャッシュを行わない
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon/1', { cache: 'no-store' })
+
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch pokemon')
+  }
+
+  return res.json()
+
+
+}
+
+export default async function Home() {
+  const pokemon = await getPokemon()
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <h1>今日はいい天気かもしれない</h1>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+      <div>homeですよ</div>
+      <div>{pokemon.name}</div>
+    </>
   )
 }
